ea6c3bf919ff9286336913a33f10114d3105eb21
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:47:16 +0600

    mahabub

---
+++ problem1.cpp
@@ -0,0 +1,20 @@
+//Lip year saal bair korar programm
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter your input number:";
+    cin>>a;
+    if(a%4==0)
+    {
+        cout<<"Lip year saal";
+    }
+    else
+    {
+        cout<<"Not lip year saal";
+    }
+    getch;
+}
