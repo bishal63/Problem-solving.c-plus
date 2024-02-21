ce9e73e2478672e5ebb70bbe5c407f0005728b8b
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:48:38 +0600

    mahabub

---
+++ problem5.cpp
@@ -0,0 +1,16 @@
+//calculating area of triangle problem solving
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b,triangle_square;
+    cout<<"Triangle land:";
+    cin>>a;
+    cout<<"Triangle height:";
+    cin>>b;
+    triangle_square=a*b/2;
+    cout<<"triangle square is "<<triangle_square<<" "<<"meter";
+    getch;
+}
