072f9e9e61b04ed41942d519b83eff182951ebd8
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 19:10:53 +0600

    mahabub

---
+++ problem6.cpp
@@ -0,0 +1,20 @@
+//even/odd number bair korar problem solving
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"enter your number:";
+    cin>>number;
+    if(number%2==0)
+    {
+        cout<<"this number is even";
+    }
+    else
+    {
+        cout<<"this number is odd";
+    }
+    getch;
+}
