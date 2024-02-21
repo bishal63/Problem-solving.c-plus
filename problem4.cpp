82666866d5a6ae8532837d2e7b96070c0412fea4
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:48:18 +0600

    mahabub

---
+++ problem4.cpp
@@ -0,0 +1,13 @@
+//100 bar hellow world print korar problem solving
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    for(int i=1;i<101;i++)
+    {
+        cout<<i<<"."<<"Hellow World!!!"<<endl;
+    }
+    getch;
+}
