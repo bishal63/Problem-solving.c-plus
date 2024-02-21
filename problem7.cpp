44619af3900f382975c818214f5284d3c39aac72
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 19:11:17 +0600

    mahabub

---
+++ problem7.cpp
@@ -0,0 +1,22 @@
+//small/large number bair korar problem solving
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"a number is:";
+    cin>>a;
+    cout<<"b number is:";
+    cin>>b;
+    if(a>b)
+    {
+        cout<<"a is larger then b";
+    }
+    else
+    {
+        cout<<"b is larger then a";
+    }
+    getch;
+}
