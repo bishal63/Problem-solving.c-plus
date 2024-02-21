b113fca8428dae7ac72aba8bcf8daea12337686b
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:47:56 +0600

    mahabub

---
+++ problem3.cpp
@@ -0,0 +1,15 @@
+//namota lekhar problem solving
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a=8;
+    for (int i=1;i<11;i++)
+    {
+        cout<<a<<"*"<<i<<"="<<a*i<<endl;
+    }
+    
+    getch;
+}
