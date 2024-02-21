8efe79fb0d04316d101970c833355813f1bea488
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 19:12:29 +0600

    mahabub

---
+++ problem10.cpp
@@ -0,0 +1,23 @@
+/*function babohar kore difernt math solution kori*/
+
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void numbers(int a,int b)
+{
+    int sum=a+b;
+    cout<<sum<<endl;
+    int sub=a-b;
+    cout<<sub<<endl;
+    int mul=a*b;
+    cout<<mul<<endl;
+    int div=a/b;
+    cout<<div;
+
+}
+int main()
+{
+    numbers(10,20);
+    getch;
+}
